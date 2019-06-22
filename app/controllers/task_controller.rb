fb8d2de8234ee1446000117be1e88eea24075ab1
ieko <mendo931411.mou@gmail.com>
2019-06-22 19:38:53 +0900

    新規タスクをデータベースに保存、表示できるようにした

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,4 +1,12 @@
 class TaskController < ApplicationController
   def index
+ @tasks = Task.all
   end
+
+  def create
+   @task = Task.new(name:params[:task_name])
+   @task.save 
+   redirect_to("/task/index")
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'task/index' => "task#index"
   get 'home/top'
-
-
+  post "task/create"=> "task#create"
 end
