159c4068338cd9ba275d1638afdbb497cc2f8499
ieko <mendo931411.mou@gmail.com>
2019-07-13 19:31:46 +0900

    コード整形

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,26 +1,26 @@
+# frozen_string_literal: true
+
 class TaskController < ApplicationController
   def index
- @tasks = Task.all
- @statuses = Status.all
- 
+    @tasks = Task.all
+    @statuses = Status.all
   end
 
   def create
-   @task = Task.new(name:params[:task_name],status_id:1)
-  @task.save 
+    @task = Task.new(name: params[:task_name], status_id: 1)
+    @task.save
 
-  #@tasks = Task.all
- # @tasks.each do |task|
-  #task.status_id=1
- #end
- #@tasks.save
+    # @tasks = Task.all
+    # @tasks.each do |task|
+    # task.status_id=1
+    # end
+    # @tasks.save
 
-   redirect_to("/task/index")
+    redirect_to("/task/index")
   end
 
   def reset
-      Task.find(1).destroy
-  redirect_to("/home/top")
+    Task.find(1).destroy
+    redirect_to("/home/top")
   end
-  
 end
